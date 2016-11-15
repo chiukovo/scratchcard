eafa0b44554577e9ef53c1e143dcedfc70c14ab6
chiuko <q8156697@gmail.com>
2016-11-15 18:21:16 +0800

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>scratchcard</title>
+    <script src="js/jquery-1.11.3.js"></script>
+    <script src="js/scratchcard.js"></script>
+</head>
+<body>
+    <div id="main">
+
+        <div><canvas id="maincanvas"></canvas></div>
+
+        <div><input id="resetbutton" type="button" value="Reset"></input></div>
+
+        <div id="canvas"></div>
+    </div>
+</body>
+</html>
